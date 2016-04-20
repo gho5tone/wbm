3ebe15ac350cce48b319c5bff2eb7ecdff47f76a
JisubChung <jisub.chung0@gmail.com>
2016-04-19 17:12:28 -0700

    Added basic functionality for a chat app using Socket.IO

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 /**
   * Everything in the public folder will be served statically
@@ -16,6 +17,17 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/html/index.html');
 });
 
+/**
+  * This is the socket server
+  * 'connection' is basically a listener (which listens to http server) for
+  * incoming socket events
+  */
+io.on('connection', function(socket) {
+  //you can make custom events such as this 'chat message'
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+});
 
 // Set server port
 http.listen(3000, function() {
---
+++ public/js/socket.js
@@ -0,0 +1,10 @@
+//dont need to specify the port number because assumes host
+var socket = io();
+$('form').submit(function() {
+  socket.emit('chat message', $('#m').val());
+  $('#m').val('');
+  return false;
+});
+socket.on('chat message', function(msg) {
+  $('#messages').append($('<li>').text(msg));
+});
